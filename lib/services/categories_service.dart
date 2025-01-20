be793d6af23608f9c5614723200e9512596b0c0d
Moheb Boules Guirges <mohebboules@gmail.com>
2025-01-20 19:12:22 +0200

    Creating services

---
+++ lib/services/categories_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:store_app/models/product_model.dart';
+
+class CategoriesService {
+  Future<List<ProductModel>> getCategoriesProducts(
+      {required String categoryName}) async {
+    List<ProductModel> productsList = [];
+    http.Response response = await http.get(
+      Uri.parse('https://fakestoreapi.com/products/category/$categoryName'),
+    );
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      for (int i = 0; i < data.length; i++) {
+        productsList.add(
+          ProductModel.fromJson(data[i]),
+        );
+      }
+      return productsList;
+    } else {
+      throw Exception('Status code error: ${response.statusCode}');
+    }
+  }
+}
--- lib/services/get_all_products_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:store_app/models/product_model.dart';
-
-class AllProductsService {
-  List<ProductModel> getAllProducts() {
-    return [];
-  }
-}
